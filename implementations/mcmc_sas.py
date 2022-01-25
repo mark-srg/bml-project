6d7415b76a6ef8d4b63a85da573add85ffa13829
SERGIEL Marc-Andre <marc-andre.sergiel@inria.fr>
2022-01-26 00:00:45 +0100

    Launching run with 1000 iterations + implementing MCMC spike-and-slab, sometimes we have negative or even zero values...

--- implementations/mcmc_sas.py
+++ implementations/mcmc_sas.py
@@ -22,64 +22,92 @@ def sample(X, sigma, rng, nu=100, pi=None, T=100):
     betas[0] = rng.normal(0,1,size=k)
     lambdas[0] = rng.normal(0,1,size=n)
     gammas[0] = rng.binomial(n=1, p=pi)
+    gamma_idx = gammas[0].astype(bool)
 
-    lambdas_idx = list(range(n))
+    lambdas_idx = list(range(n))    
     
-    # We need to initialize B and b
+    B_gamma_inv = (1/nu**2) * sigma2_inv[gamma_idx,:][:,gamma_idx] + X[:,gamma_idx].T @ np.diag(1/lambdas[0]) @ X[:,gamma_idx]
+    B_gamma = np.linalg.inv(B_gamma_inv)
+    b_gamma = B_gamma @ X[:,gamma_idx].T @ (np.ones(n) + 1/lambdas[0])
 
     for t in range(1,T):
         
         if t % 100 == 0:
             print("Step {}".format(t))
         
-        # Do we need to keep that part ? I would say yes just to get rid of the support vectors
-        # num_lambda = numerical(1/lambdas[t-1])
-        # if num_lambda[0]:
-        #     print("At time", t)
-        #     idx, tag = num_lambda[1], num_lambda[2]
-        #     print("Found a numerical issue for lambda, in position {}, with value {}".format(i,tag))
-        #     print("Support vector found at index {}".format(i))
-        #     lambdas[:,idx] = np.nan
-        #     for i in idx:
-        #         if i in lambdas_idx:
-        #             lambdas_idx.remove(i)
-        #     print('Remaining coeffs in lambda : {}'.format(len(lambdas_idx)))
+        num_lambda = numerical(1/lambdas[t-1])
+        if num_lambda[0]:
+            print("At time", t)
+            idx, tag = num_lambda[1], num_lambda[2]
+            print("Found a numerical issue for lambda, in position {}, with value {}".format(i,tag))
+            print("Support vector found at index {}".format(i))
+            lambdas[:,idx] = np.nan
+            for i in idx:
+                if i in lambdas_idx:
+                    lambdas_idx.remove(i)
+            print('Remaining coeffs in lambda : {}'.format(len(lambdas_idx)))
             
         # Sampling lambda using beta
-        gamma_idx = gammas[t-1].astype(bool)
         for i in lambdas_idx:
-            lambda_inv = rng.wald(1/np.abs(1-X[i][gamma_idx].T @ betas[t][gamma_idx]), 1)
+            lambda_inv = rng.wald(1/np.abs(1-X[i][gamma_idx].T @ betas[t-1][gamma_idx]), 1)
             if lambda_inv == 0.0:
                 lambdas[t,i] = np.inf
             else:
                 lambdas[t,i] = 1/lambda_inv
+                
+        # Compute XΛ^(−1)X and X.T(1+λ^(−1)) to avoid recomputing them
+        reusable_result1 = X[lambdas_idx,:].T @ np.diag(1/lambdas[t-1][lambdas_idx]) @ X[lambdas_idx,:]
+        reusable_result2 = X[lambdas_idx,:].T @ (np.ones(len(lambdas_idx)) + 1/lambdas[t-1][lambdas_idx])
                                                   
         # Sampling gamma using newest available values
         current_gamma = np.copy(gammas[t-1])
         for j in range(k):
             p = np.zeros(2)
-            for value in values:
-                current_gamma[j] = value
-                gamma_idx = current_gamma.astype(bool) 
-                
-                term1 = np.array([pi[i]**current_gamma[i] * (1-pi[i])**(1-current_gamma[i]) for i in range(k)]).prod()
-                term2 = np.sqrt( (1/nu**2) * (1/sigma[gamma_idx]**2).prod() / np.linalg.det(B_inv[gamma_idx]))
-                term3 = np.exp(- (1/2) * ( b[gammai_dx].T @ X[gamma_idx,:][:,gamma_idx].T @ np.diag(1/lambdas[t-1][lambdas_idx]) @ X[gamma_idx,:][:,gamma_idx] @ b[gamma_idx] - 2 * b[gamma_idx].T @ X[gamma_idx,:][:,gamma_idx].T @ (np.ones(len(lambdas_idx)) + 1/lambdas[t-1][lambdas_idx]) - (1/(2*nu**2)) * b[gamma_idx].T @ sigma2_inv[gamma_idx,:][:,gamma_idx] @ b[gamma_idx] ))
-                
-                p[value] = term1 * term2 * term3
-                
+            
+            current_gamma_0 = np.copy(gammas[t-1])
+            current_gamma_0[j] = 0
+            gamma_idx_0 = current_gamma_0.astype(bool)
+            current_gamma_1 = np.copy(gammas[t-1])
+            current_gamma_1[j] = 1
+            gamma_idx_1 = current_gamma_1.astype(bool)
+            
+            current_gammas = [current_gamma_0, current_gamma_1]
+            gamma_idxs = [gamma_idx_0, gamma_idx_1]
+            
+            B_gamma_inv_0 = (1/nu**2) * sigma2_inv[gamma_idx_0,:][:,gamma_idx_0] + reusable_result1[gamma_idx_0,:][:,gamma_idx_0]
+            B_gamma_0 = np.linalg.inv(B_gamma_inv_0)
+            b_gamma_0 = B_gamma_0 @ reusable_result2[gamma_idx_0]
+            B_gamma_inv_1 = (1/nu**2) * sigma2_inv[gamma_idx_1,:][:,gamma_idx_1] + reusable_result1[gamma_idx_1,:][:,gamma_idx_1]
+            B_gamma_1 = np.linalg.inv(B_gamma_inv_1)
+            b_gamma_1 = B_gamma_1 @ reusable_result2[gamma_idx_1]
+            
+            B_gammas = [B_gamma_0, B_gamma_1]
+            b_gammas = [b_gamma_0, b_gamma_1]
+            
+            term1_0 = np.array([(pi[i]**current_gamma_0[i]) * ((1-pi[i])**(1-current_gamma_0[i])) for i in range(k)]).prod()
+            # Determinant of B_inv can be negative... Fixed using np.abs but I'm not sure... 
+            term2_0 = np.sqrt((1/nu**2) * (1/sigma[gamma_idx_0]**2).prod() / np.linalg.det(B_gamma_inv_0))
+            term3_0 = np.exp(- (1/2) * ( b_gamma_0.T @ reusable_result1[gamma_idx_0,:][:,gamma_idx_0] @ b_gamma_0 - 2 * b_gamma_0.T @ reusable_result2[gamma_idx_0] - (1/(nu**2)) * b_gamma_0.T @ sigma2_inv[gamma_idx_0,:][:,gamma_idx_0] @ b_gamma_0 ))
+            p[0] = term1_0 * term2_0 * term3_0
+            
+            term1_1 = np.array([(pi[i]**current_gamma_1[i]) * ((1-pi[i])**(1-current_gamma_1[i])) for i in range(k)]).prod()
+            term2_1 = np.sqrt((1/nu**2) * (1/sigma[gamma_idx_1]**2).prod() / np.linalg.det(B_gamma_inv_1))
+            term3_1 = np.exp(- (1/2) * ( b_gamma_1.T @ reusable_result1[gamma_idx_1,:][:,gamma_idx_1] @ b_gamma_1 - 2 * b_gamma_1.T @ reusable_result2[gamma_idx_1] - (1/(nu**2)) * b_gamma_1.T @ sigma2_inv[gamma_idx_1,:][:,gamma_idx_1] @ b_gamma_1 ))
+            p[1] = term1_1 * term2_1 * term3_1
+            
+            print(p)
+            
             p = p / p.sum()
             
-            gammas[t,j] = rng.choice(values, p=p)
+            chosen_value = rng.choice(a=np.arange(2).astype(int), p=p)
+            current_gamma = current_gammas[chosen_value]
+            gammas[t] = np.copy(current_gamma)
+            
+            B_gamma = B_gammas[chosen_value]
+            b_gamma = b_gammas[chosen_value]
+            gamma_idx = gamma_idxs[chosen_value]
 
-        # Computing B and b
-        term1 = nu**(-2)* sigma2_inv[omegas_idx,:][:,omegas_idx] @ np.diag(1/omegas[t-1][omegas_idx])
-        term2 = X[lambdas_idx,:][:,omegas_idx].T @ np.diag(1/lambdas[t-1][lambdas_idx]) @ X[lambdas_idx,:][:,omegas_idx]
-        B_inv = term1 + term2
-        B = np.linalg.inv(B_inv)
-        b = B @ X[lambdas_idx,:][:,omegas_idx].T @ (np.ones(len(lambdas_idx)) + 1/lambdas[t-1][lambdas_idx])
-
-        # Sampling beta using previous state
-        betas[t][omegas_idx] = rng.multivariate_normal(b, cov=B)
+        # Sampling beta using newest available values
+        betas[t][gamma_idx] = rng.multivariate_normal(b_gamma, cov=B_gamma)
     
     return betas
